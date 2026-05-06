6fd4e29c2bf60d651333602996f53227e638dca8
GioEspinoza <giovanni.espinoza0@gmail.com>
2026-05-05 22:18:54 -0400

    test

--- src/futcomp/threadstest.java
+++ src/futcomp/threadstest.java
@@ -5,8 +5,8 @@ public class threadstest {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Main thread starts");
 
-        // A thread is a separate path of work.
-        // This lets the program do more than one thing at the same time.
+        // A thread is a separate path for background work
+        // letting the program do more than one thing at the same time.
         Thread workerOne = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -18,8 +18,8 @@ public class threadstest {
         // A lambda is just a shorter way to write a Runnable.
         Thread workerTwo = new Thread(() -> count("Worker 2"));
 
-        // start() tells Java to run the thread in the background.
-        // Do not use run() here. run() would act like a normal method call.
+        // start() tells Java to run the thread in the background
+        //run() would act like a normal method call.
         workerOne.start();
         workerTwo.start();
 
